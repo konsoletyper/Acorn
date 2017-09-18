c287437e5c55bb2b9a146900186d0e11d34da06d
nbilyk <nick@nbilyk.com>
2017-09-18 10:45:21 -0500

    debugging style calculator

--- AcornUiCore/src/com/acornui/component/style/StyleCalculator.kt
+++ AcornUiCore/src/com/acornui/component/style/StyleCalculator.kt
@@ -3,6 +3,7 @@ package com.acornui.component.style
 import com.acornui.collection.addSorted
 import com.acornui.collection.forEach2
 import com.acornui.collection.forEachReversed2
+import com.acornui.collection.sortedInsertionIndex
 
 /**
  * A style calculator is responsible for setting the calculated values
@@ -23,11 +24,8 @@ object CascadingStyleCalculator : StyleCalculator {
 	}
 
 	override fun calculate(style: Style, target: StyleableRo) {
-		calculated.clear()
-
 		// Collect all style rule objects for the bound style type and tags.
 		// These entries will be sorted first by priority, and then by ancestry level.
-		entries.clear()
 		target.walkStyleableAncestry {
 			ancestor ->
 			style.type.walkInheritance {
@@ -44,8 +42,7 @@ object CascadingStyleCalculator : StyleCalculator {
 
 		// Apply style entries to the calculated values of the bound style.
 		entries.forEach2 {
-			entry ->
-			for (e in entry.style.explicit) {
+			for (e in it.style.explicit) {
 				if (!calculated.containsKey(e.key)) {
 					calculated[e.key] = e.value
 				}
@@ -56,5 +53,69 @@ object CascadingStyleCalculator : StyleCalculator {
 			style.calculated.putAll(calculated)
 			style.modTag.increment()
 		}
+		calculated.clear()
+		entries.clear()
+	}
+
+	fun getDebugInfo(style: Style, target: StyleableRo): List<StyleRuleDebugInfo> {
+		// Collect all style rule objects for the bound style type and tags.
+		// These entries will be sorted first by priority, and then by ancestry level.
+		val appliedRules = ArrayList<StyleRuleDebugInfo>()
+		target.walkStyleableAncestry {
+			ancestor ->
+			style.type.walkInheritance {
+				styleType ->
+				ancestor.getRulesByType(styleType, tmp)
+				tmp.forEachReversed2 {
+					entry ->
+					if (entry.filter(target) != null) {
+						val index = entries.sortedInsertionIndex(entry, entrySortComparator)
+						entries.add(index, entry)
+						appliedRules.add(index, StyleRuleDebugInfo(ancestor, entry))
+					}
+				}
+			}
+		}
+
+		// Apply style entries to the calculated values of the bound style.
+		for (i in 0..entries.lastIndex) {
+			val entry = entries[i]
+			val ruleInfo = appliedRules[i]
+			for (e in entry.style.explicit) {
+				if (!calculated.containsKey(e.key)) {
+					calculated[e.key] = e.value
+					ruleInfo.calculated[e.key] = e.value
+				}
+			}
+		}
+
+		calculated.clear()
+		entries.clear()
+		return appliedRules
+	}
+}
+
+fun List<StyleRuleDebugInfo>.prettyPrint(): String {
+	var str = ""
+	for (styleRuleDebugInfo in this) {
+		if (str.isNotEmpty()) str += ", \n"
+		str += styleRuleDebugInfo.prettyPrint()
+	}
+	return str
+}
+
+class StyleRuleDebugInfo(
+		val ancestor: StyleableRo,
+		val entry: StyleRule<*>) {
+
+	val calculated: MutableMap<String, Any?> = HashMap()
+
+	fun prettyPrint(): String {
+		var str = "$ancestor ${entry.filter} ${entry.priority} {\n"
+		for ((key, value) in calculated) {
+			str += "	$key = $value\n"
+		}
+		str += "}"
+		return str
 	}
 }
